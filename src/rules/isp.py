c6bf5b3df8bcc66761abca5cdf5c1609f287a3c2
Michal Cygan <cyganmichal.uxui@gmail.com>
2025-06-04 11:30:42 +0200

    Labolatorium 5 i 6

---
+++ src/rules/isp.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class IWork(ABC):
+    @abstractmethod
+    def work(self):
+        pass
+
+class IEat(ABC):
+    @abstractmethod
+    def eat(self):
+        pass
+
+class HumanWorker(IWork, IEat):
+    def work(self):
+        print("Human working")
+
+    def eat(self):
+        print("Human eating")
+
+class RobotWorker(IWork):
+    def work(self):
+        print("Robot working")
+
+# Użycie
+human = HumanWorker()
+robot = RobotWorker()
+
+human.work()
+human.eat()
+robot.work()
